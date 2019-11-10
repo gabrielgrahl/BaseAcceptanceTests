4c5a3e0735cf2f11ceafdedb1313b9b4bfd01f41
gabrielgrahl <gabrielgrahl@gmail.com>
2019-11-10 13:29:16 -0300

    Initial commit

---
+++ AcceptanceTests/Features/Calculator.feature
@@ -0,0 +1,9 @@
+﻿#language: pt-BR
+Funcionalidade: Calculadora
+
+
+Cenario: Add two numbers
+	Dado I have entered 50 into the calculator
+	E I have entered 70 into the calculator
+	Quando I press add
+	Entao the result should be 120 on the screen
---
+++ AcceptanceTests/Steps/CalculadoraSteps.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.Steps
+{
+    [Binding]
+    public class CalculadoraSteps
+    {
+        private int _soma;
+        [Given(@"I have entered (.*) into the calculator")]
+        public void DadoIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _soma += p0;
+        }
+        
+        [When(@"I press add")]
+        public void QuandoIPressAdd()
+        {
+            
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void EntaoTheResultShouldBeOnTheScreen(int p0)
+        {
+            p0.Should().Be(_soma);
+        }
+    }
+}
